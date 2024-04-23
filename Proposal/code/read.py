92e6d9d7bf7b6feb7beff0403cf74da5f666dcf2
Jinyuan Huang <88757735+BernieHuang2008@users.noreply.github.com>
2024-04-23 08:29:08 +0000

    add read_md

--- Proposal/code/read.py
+++ Proposal/code/read.py
@@ -1,12 +1,28 @@
 import os
+import markdown
 
 
 def readfile(number: int):
     dirname = f"Proposal/OTP-{number}"
     dir = os.listdir(dirname)
+    cfg = dict()
+
     if 'index.html' in dir:
-        pass
-    if 'index.md' in dir:
-        pass
-    
+        read_html(dirname)
+    elif 'index.md' in dir:
+        read_md(dirname)
+
+
+def read_html(dirname):
+    pass
+
+
+def read_md(dirname):
+    with open(f"{dirname}/index.md") as f:
+        mdcode = f.read()
+    htmlcode = markdown.markdown(mdcode)
+    with open(f"{dirname}/index.html", 'w') as f:
+        f.write(htmlcode)
+
+    return read_html(dirname)
 
